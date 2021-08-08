7b9d1f3b5e4f7e2e2d006dde2934770b0640f7df
muralikrishnan MS <77663028+msmurali@users.noreply.github.com>
2021-08-08 14:04:10 +0530

    updated grid.jsx

--- src/components/grid/grid.jsx
+++ src/components/grid/grid.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import "./grid.css";
 import { WeatherContext } from "../weather/weather";
 import to12Hrformat from "../../services/dateTime.js";
